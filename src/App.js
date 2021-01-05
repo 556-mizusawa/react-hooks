f51af246a980e3dd3157145c82a02ff0485ec659
556 <68994528+556-mizusawa@users.noreply.github.com>
2021-01-05 17:08:26 +0900

    react Hook with useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
-  return <div>This is a template for React app.</div>;
+  const initialCount = 0;
+  const [count, setCount] = useState(initialCount);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  const reset = () => setCount(initialCount);
+
+  const increment2 = () => setCount((previousCount) => previousCount + 1);
+  const decrement2 = () => setCount((previousCount) => previousCount - 1);
+
+  const double = () => setCount(count * 2);
+  const divide3 = () =>
+    setCount((previousCount) =>
+      previousCount % 3 === 0 ? previousCount / 3 : previousCount
+    );
+
+  return (
+    <>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+        <button onClick={reset}>Reset</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={double}>x2</button>
+        <button onClick={divide3}>÷3</button>
+      </div>
+    </>
+  );
 };
 
 export default App;
